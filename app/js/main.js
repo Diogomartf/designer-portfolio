168053bcae9d62ad2b9a597d5b5897f85f48709a
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-10-10 23:50:05 +0100

    Fixed social icons center, improved design

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,39 @@ $(".menu-collapsed").click(function() {
 $(document).ready(function(){
    $('.scrollspy').scrollSpy();
  });
+
+ $(".link-insta").hover(
+   function () {
+     $("#insta").addClass('animated bounce');
+   },
+   function () {
+     $("#insta").removeClass("animated bounce");
+   }
+ );
+
+ $(".link-dribble").hover(
+   function () {
+     $("#dribble").addClass('animated bounce');
+   },
+   function () {
+     $("#dribble").removeClass("animated bounce");
+   }
+ );
+
+ $(".link-twitter").hover(
+   function () {
+     $("#twitter").addClass('animated bounce');
+   },
+   function () {
+     $("#twitter").removeClass("animated bounce");
+   }
+ );
+
+ $(".link-linkedin").hover(
+   function () {
+     $("#linkedin").addClass('animated bounce');
+   },
+   function () {
+     $("#linkedin").removeClass("animated bounce");
+   }
+ );
